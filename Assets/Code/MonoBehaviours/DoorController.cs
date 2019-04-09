ec90ebc833f7c971e226bfed970b6c8bcdd9c749
englmantin <gonzoglizli@gmail.com>
2019-04-09 11:16:45 +0200

    * implemented basic door open/close mechanic

---
+++ Assets/Code/MonoBehaviours/DoorController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorController : MonoBehaviour
+{
+    [SerializeField]
+    private float ClosedDoorY = 1.95f;
+    [SerializeField]
+    private float OpenedDoorY = 1.95f;
+    [SerializeField]
+    private float DoorSpeed = 0.1f;
+
+    private bool _isOpen = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _isOpen = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float doorYDiff = 0;
+        if (_isOpen && transform.position.y > OpenedDoorY)
+        {
+            doorYDiff = -Time.deltaTime * DoorSpeed;
+        }
+
+        if (!_isOpen && transform.position.y < OpenedDoorY)
+        {
+            doorYDiff = Time.deltaTime * DoorSpeed;
+        }
+
+        if  (doorYDiff != 0)
+        {
+            transform.Translate (new Vector3(0, doorYDiff, 0));
+        }
+    }
+
+    public void SetOpen(bool isOpen)
+    {
+        _isOpen = isOpen;
+    }
+}
--- Assets/Code/MonoBehaviours/LiftController.cs
+++ Assets/Code/MonoBehaviours/LiftController.cs
@@ -2,15 +2,18 @@
 
 public class LiftController : MonoBehaviour
 {
+
+
     [SerializeField]
     private int _sceneIndex = 0;
     [SerializeField]
-    private GameObject _frontWall = null;
-
+    private DoorController _liftDoor = null;
+    
     public ISceneLoader SceneLoader { get; set; }
 
     private void OnTriggerEnter(Collider other)
     {
-        SceneLoader.LoadScene(_sceneIndex);
+        //SceneLoader.LoadScene(_sceneIndex);
+        _liftDoor.SetOpen(false);
     }
 }
