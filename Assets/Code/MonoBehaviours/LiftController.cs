094915a4cf04aacf90b355ed7f47ed0ca1b7b7d0
englmantin <gonzoglizli@gmail.com>
2019-04-09 15:31:00 +0200

    * lift prefab update  * small refactor

--- Assets/Code/MonoBehaviours/LiftController.cs
+++ Assets/Code/MonoBehaviours/LiftController.cs
@@ -6,7 +6,9 @@ public class LiftController : MonoBehaviour
     private int _sceneIndex = 0;
     [SerializeField]
     private DoorController _liftDoor = null;
-    
+    [SerializeField]
+    private DoorController _liftDoorPannel = null;
+
     public ISceneLoader SceneLoader { get; set; }
 
     private void OnTriggerEnter(Collider other)
