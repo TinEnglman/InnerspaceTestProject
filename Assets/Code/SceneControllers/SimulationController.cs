a83dde748988da3134afe54db775dace2303b2f5
englmantin <gonzoglizli@gmail.com>
2019-04-10 11:11:58 +0200

    * fixed broken prefabs

---
+++ Assets/Code/SceneControllers/SimulationController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimulationController : MonoBehaviour
+{
+    [SerializeField]
+    private string _textFolderName = "Texts";
+    [SerializeField]
+    private LiftExitController _liftController = null;
+
+    void Start()
+    {
+        LocalizationManager.Instance.TextLoader = new TextLoader();
+        LocalizationManager.Instance.LoadData(_textFolderName);
+    }
+}
